72d7fb741d5a433758b15c6a483a3ba134723935
y-saeki <y-saeki@pyramid-inc.net>
2018-10-30 17:44:36 +0900

    特定のブランチ上でのコミットと抽出するshファイル変更（コミッター指定を追加）

--- commit-diff.sh
+++ commit-diff.sh
@@ -4,14 +4,16 @@ set -eu
 diff_branch=develop-diff
 develop_branch=develop
 stage_branch=stage
+for committer in y-saeki yo-suzuki oomori koyama 
+do
+  committers+=("--committer=$committer")
+done
 
 git checkout $diff_branch
 
 from=$(git log --merges -1 --format=%H)
-#echo $from
 
-#git rev-list --reverse --topo-order --no-merges --committer=y-saeki --ancestry-path $diff_branch^2..$develop_branch | while read rev 
-(git rev-list --reverse --topo-order --no-merges $from^2..$develop_branch --ancestry-path | cat -n; git rev-list --reverse --topo-order --no-merges $from^2..$stage_branch --first-parent | cat -n) | sort -k2 | uniq -f1 -u | sort -n | cut -f2 | while read rev 
+(git rev-list --reverse --topo-order --no-merges $from^2..$develop_branch ${committers[@]} --ancestry-path | cat -n; git rev-list --reverse --topo-order --no-merges $from^2..$stage_branch --first-parent | cat -n) | sort -k2 | uniq -f1 -u | sort -n | cut -f2 | while read rev 
 do
   #git log -1 --oneline $rev || break
   git cherry-pick -Xtheirs -x $rev || break 
