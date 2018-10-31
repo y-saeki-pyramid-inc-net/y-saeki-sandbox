ad1bb8ccbc1fc9b85e287d2aa3be0ab4e76b910e
y-saeki <y-saeki@pyramid-inc.net>
2018-10-31 19:08:06 +0900

    特定のブランチ上でのコミットと抽出するshファイル変更

--- commit-diff.sh
+++ commit-diff.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -eu
 
-diff_branch=develop-diff
+find_merge () {
+  local commit=$1
+  local branch=${2:-HEAD}
+  (git rev-list $commit..$branch --ancestry-path | cat -n; git rev-list $commit..$branch --first-parent | cat -n) | sort -k2 | uniq -f1 -d | sort -n | tail -1 | cut -f2
+  return 0
+}
+
+diff_branch=$(git rev-parse --abbrev-ref HEAD)
 develop_branch=develop
+#develop_from=$(find_merge $develop_branch $diff_branch) 
+#if [ -z "$develop_from" ]; then
+#  develop_from=$diff_branch
+#fi
 stage_branch=stage
+#stage_from=$(find_merge $stage_branch $develop_branch)
 for committer in y-saeki yo-suzuki oomori koyama 
 do
   committers+=("--committer=$committer")
 done
 
-git checkout $diff_branch
-
-from=$(git log --merges -1 --format=%H)
-
-(git rev-list --reverse --topo-order --no-merges $from^2..$develop_branch ${committers[@]} --ancestry-path | cat -n; git rev-list --reverse --topo-order --no-merges $from^2..$stage_branch --first-parent | cat -n) | sort -k2 | uniq -f1 -u | sort -n | cut -f2 | while read rev 
+#(git rev-list --reverse --topo-order --no-merges $develop_from^2..$develop_branch ${committers[@]} --ancestry-path | cat -n; git rev-list --reverse --topo-order --no-merges $stage_from^2..$stage_branch --first-parent | cat -n) | sort -k2 | uniq -f1 -u | sort -n | cut -f2 | while read rev
+git rev-list --reverse --topo-order $develop_branch ^$diff_branch ^$stage_branch ${committers[@]} | while read rev
 do
   #git log -1 --oneline $rev || break
   git cherry-pick -Xtheirs -x $rev || break 
