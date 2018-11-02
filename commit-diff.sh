2e669aff95dcd12491772bb7af38a492c59bf005
y-saeki <y-saeki@pyramid-inc.net>
2018-11-02 10:10:53 +0900

    特定のブランチ上でのコミットと抽出するshファイル変更

--- commit-diff.sh
+++ commit-diff.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-set -eu
+set -u
+
+pushd $(cd $(dirname $0); pwd) > /dev/null
+
+DEVELOP_DIFF="develop-diff"
+FROM=$DEVELOP_DIFF
+DEVELOP="develop"
+STAGE="stage"
+IS_MERGE=false
+#diff_branch=$(git rev-parse --abbrev-ref HEAD)
+for committer in y-saeki yo-suzuki oomori koyama 
+do
+  COMMITTERS+=("--committer=$committer")
+done
+
+usage_exit() {
+  echo "Usage: `basename $0` [-f] [-d] [-s] [-m]" 1>&2
+  echo "[-f] : start commit. default is '${DEVELOP_DIFF}'" 1>&2
+  echo "[-d] : develop branch. default is '${DEVELOP}'" 1>&2
+  echo "[-s] : stage branch. default is '${STAGE}'" 1>&2
+  echo "[-m] : create merge commit on ${DEVELOP_DIFF} branch after execute. default is '${IS_MERGE}'" 1>&2
+  echo "" 1>&2
+  exit 1
+}
+
+while getopts "f:d:s:m" opt; 
+do
+  case "$opt" in
+    d) DEVELOP="${OPTARG}" ;;
+    s) STAGE="${OPTARG}" ;;
+    m) IS_MERGE="true" ;;
+    h)  usage_exit ;;
+    \?) usage_exit ;;
+  esac
+done
 
 find_merge () {
   local commit=$1
@@ -8,24 +42,22 @@ find_merge () {
   return 0
 }
 
-diff_branch=$(git rev-parse --abbrev-ref HEAD)
-develop_branch=develop
-#develop_from=$(find_merge $develop_branch $diff_branch) 
-#if [ -z "$develop_from" ]; then
-#  develop_from=$diff_branch
-#fi
-stage_branch=stage
-#stage_from=$(find_merge $stage_branch $develop_branch)
-for committer in y-saeki yo-suzuki oomori koyama 
-do
-  committers+=("--committer=$committer")
-done
+make_message () {
+  local MESSAGE="Differents commits are"
+  while read rev
+  do
+    MESSAGE=$MESSAGE"\n"$(git log -1 --pretty=format:"%H %an : %s" $rev)
+  done
+  echo $MESSAGE
+  return 0
+}
 
-#(git rev-list --reverse --topo-order --no-merges $develop_from^2..$develop_branch ${committers[@]} --ancestry-path | cat -n; git rev-list --reverse --topo-order --no-merges $stage_from^2..$stage_branch --first-parent | cat -n) | sort -k2 | uniq -f1 -u | sort -n | cut -f2 | while read rev
-git rev-list --reverse --topo-order $develop_branch ^$diff_branch ^$stage_branch ${committers[@]} | while read rev
-do
-  #git log -1 --oneline $rev || break
-  git cherry-pick -Xtheirs -x $rev || break 
-done 
+MESSAGE=$(git rev-list --reverse --topo-order $DEVELOP ^$FROM ^$STAGE ${COMMITTERS[@]} | make_message)
+echo "$MESSAGE"
 
-git merge --no-ff --log -m "Merge branch '${develop_branch}' into ${diff_branch}" $develop_branch
+if $IS_MERGE; then
+  git checkout $DEVELOP_DIFF || exit $?
+  git merge --no-ff -m "$MESSAGE" $DEVELOP
+fi
+
+popd > /dev/null
